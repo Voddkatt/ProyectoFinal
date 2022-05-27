864304cacd3ab9a538741694fb16f4dcc9175aa7
Voddkatt <voddkatt@gmail.com>
2022-05-27 12:40:30 -0400

    Segundo cambio Django

--- App/core/views.py
+++ App/core/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+
+
+    datos = {
+        'nombre': 'SuperAdmin', 
+        'numero':[1,2,3]
+    }
+
+    return render(request,'core/home.html',datos)
+
+    
