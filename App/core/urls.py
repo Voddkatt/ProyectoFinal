055524cb20307f77fd4d2ef35151d6c56a96a1bf
Voddkatt <voddkatt@gmail.com>
2022-06-10 12:38:13 -0400

    Último commit Django

--- App/core/urls.py
+++ App/core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path 
-from .views import index ,formulario, login, suscripcion, productos, form_productos
+from .views import index ,formulario, login, suscripcion, productos, form_productos, form_mod_productos, form_del_productos
 
 urlpatterns = [
     path('', index, name="index"),
@@ -8,4 +8,6 @@ urlpatterns = [
     path('suscripcion', suscripcion, name="sus"),
     path('productos', productos, name='prod'),
     path('form-productos', form_productos, name="form_productos"),
+    path('form-mod-productos/<id>' , form_mod_productos, name="form_mod_productos"),
+    path('form-del_productos/<id>', form_del_productos, name="form_del_productos"),
 ]
--- App/core/views.py
+++ App/core/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Productos
+from django.shortcuts import render, redirect
+from .models import *
+from .forms import *
 
 # Create your views here.
 
@@ -29,8 +30,30 @@ def productos(request):
     datos = {
         'productos': productos
     }
-    return render(request, 'core/productos.html')
+    return render(request, 'core/productos.html',datos)
 
 def form_productos(request):
     
-    return render(request, 'core/form_productos.html')
+    datos = {
+        'form': ProductosForm()
+    }
+    if request.method== 'POST':
+        formulario = ProductosForm(request.POST)
+        if formulario.is_valid:
+            formulario.save()
+            datos['mensaje'] = 'Guardados correctamente'
+
+    return render(request, 'core/form_productos.html', datos)
+
+def form_mod_productos(request, id):
+    productos = Productos.objects.get(producto=id)
+    datos = {
+        'form': ProductosForm(instance=productos)
+    }
+
+    return render(request, 'core/form_mod_productos.html', datos)
+
+def form_del_productos(request, id):
+    productos = Productos.objects.get(producto=id)
+    productos.delete()
+    return redirect(to="productos")
